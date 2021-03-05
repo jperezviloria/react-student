18e96a28a239810cbb197487ea2b3522032809ea
eperez <eperez@huenei.com>
2021-03-05 10:38:51 -0300

    implement axios in student form

--- src/components/form-student/form-student.jsx
+++ src/components/form-student/form-student.jsx
@@ -1,13 +1,53 @@
-import React from "react"
+import React, {useState} from "react"
+import {useForm} from "react-hook-form"
 import "./style.css"
+import axios from "axios"
 
 function FormStudent(){
+
+    const {register, handleSubmit} = useForm();
+    const [student, setStudent] = useState({name: "", age: ""})
+
+    const callingSaveStudent = async (data) => {
+        console.log(data)
+        var studentModel = {
+            nameStudent : data.nameStudent,
+            ageStudent : data.ageStudent
+        }
+        console.log(studentModel)
+        axios.post("/student", studentModel)
+        .catch(error =>{
+            console.log(error)
+        })
+    }
+
+    const saveStudent = (data) => {
+        //setStudent(data) Dont work
+        callingSaveStudent(data)
+    }
+
+    const onSubmitInsertStudent = (data) => {
+        saveStudent(data)
+        setStudent({name: "", age: ""})
+    }
+
     return(
         <div className="complete-form">
             <h1>Save student</h1>
-            <form className="particular-form">
-                <input type="text" placeholder="name student"/>
-                <input type="number" placeholder="age student"/>
+            <form onSubmit={handleSubmit(onSubmitInsertStudent)} className="particular-form">
+                <input 
+                type="text" 
+                placeholder="name student" 
+                name= "nameStudent"
+                ref={register}
+                //onChange={e => setStudent(e.target.value)}
+                //value={student.nameStudent}
+                />
+                <input 
+                type="number" 
+                placeholder="age student" 
+                name= "ageStudent"
+                ref={register}/>
                 <button type="submit"> save </button>
             </form>
             <img src="https://i.imgur.com/Ih8XMS3.png" alt=""/>
